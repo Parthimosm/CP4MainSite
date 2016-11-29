29d380c48e4aabadaab80db314120ef5d7ab2611
Matthew <matthew.parthimos18@alumni.loyola.ca>
2016-11-29 11:40:16 -0500

    Rearranged Files/phone mode

---
+++ GOALS/Script/javascript/jScript.js
@@ -0,0 +1,8 @@
+	function confirmTerms() {
+			var subscribe = confirm("AGREE TO THE TERMS AND CONDITIONS?");
+			if (subscribe == true) {
+				alert("Thank you for signing up!");
+			} else {
+				alert("You're missing out!");
+			}	
+		}
