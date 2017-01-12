c5acfd75d60cd26bd3377ae2f34cb621e3d89eab
Matthew <matthew.parthimos18@alumni.loyola.ca>
2017-01-12 11:08:52 -0500

    serterye

--- GOALS/Script/javascript/phoneMode.js
+++ GOALS/Script/javascript/phoneMode.js
@@ -1,18 +1,4 @@
-function dropDown() {
-    document.getElementByClassName("drpop-div-phone").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropdown-phone')) {
-
-    var dropdowns = document.getElementsByClassName("drpop-div-phone");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
+function hideElement(){
+	document.getElementById("drpop-div-phone").style.visibility = "hidden";
+	
 }
