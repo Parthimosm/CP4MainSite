a741706a78623a509f39fb676bf34594dff3fe8b
Matthew <matthew.parthimos18@alumni.loyola.ca>
2017-04-02 14:06:35 -0400

    Update jScript.js

--- GOALS/Script/javascript/jScript.js
+++ GOALS/Script/javascript/jScript.js
@@ -13,9 +13,11 @@ function Login() {
     var username = prompt("Please input your user name");
     /*defines password*/
     var pass = prompt("Please input your password");
+    /*password and username arrays*/
     var userArray = ["Guy", "Guy1", "Guy2", "Guy3", "Guy4", "Guy5"];
     var passArray = ["pass", "pass1", "pass2", "pass3", "pass4", "pass5"];
     
+    /* For loop checks both arrays to see if username matches password, must have same amount of usernames and passwords in array*/
     for (var i = 0; i < userArray.length; i++) {
         if ((username === userArray[i]) && (pass === passArray[i])) {
             valid = true;
@@ -25,6 +27,7 @@ function Login() {
         }
     }
     
+    /*changes html element if true*/
     if (valid === true) {
         document.getElementById("Username").innerHTML = "Welcome" + " " + userArray[i];
     }
