227cb5b3c7a4309b1182b56a1234543198cfd241
Matthew <matthew.parthimos18@alumni.loyola.ca>
2017-05-03 11:11:52 -0400

    oops
    trying to make multiple accounts

--- GOALS/Script/javascript/userCreator.js
+++ GOALS/Script/javascript/userCreator.js
@@ -1,6 +1,5 @@
-myStorage = localStorage;
 
-
+ 
 function profile(firstIn, lastIn, userIn, emailIn, passIn){
 	this.firstName = firstIn;
 	this.lastName = lastIn;
@@ -10,11 +9,14 @@ function profile(firstIn, lastIn, userIn, emailIn, passIn){
 };
 
 function profcreate(){
+	var i = 0
+	var profNumber = ["p1", "p2", "p3", "p4", "p5", "p6", "p7"]
 	var in1 = document.getElementById("fn").value
 	var in2 = document.getElementById("ln").value
 	var in3 = document.getElementById("un").value
 	var in4 = document.getElementById("email").value
 	var in5 = document.getElementById("pass").value
-	var profile1 = new profile(in1, in2, in3, in4, in5)
-	console.log(profile1)
+	profNumber[i] = new profile(in1, in2, in3, in4, in5)
+	console.log(profNumber[i])
+	i++
 }
