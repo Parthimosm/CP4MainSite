aae8556eaedb3a62e6b831c9d83c85372fedac7b
Matthew <matthew.parthimos18@alumni.loyola.ca>
2017-05-12 14:00:49 -0400

    object login
    in progress

--- GOALS/Script/javascript/userCreator.js
+++ GOALS/Script/javascript/userCreator.js
@@ -9,7 +9,7 @@ function profile(firstIn, lastIn, userIn, emailIn, passIn){
 	this.passWord = passIn;
 };
 
-id = 1
+
 /*
  
 function profile(){
@@ -17,36 +17,49 @@ function profile(){
 	
 	
 }*/
-var profNumber = []
+	var profNumber = [];
+	
 function profcreate(){
 	
-	console.log(profNumber);
+	
 	var in1 = document.getElementById("fn").value
 	var in2 = document.getElementById("ln").value
 	var in3 = document.getElementById("un").value
 	var in4 = document.getElementById("email").value
 	var in5 = document.getElementById("pass").value
+	
 	var userProfile = new profile(in1, in2, in3, in4, in5)
-	profNumber.push(userProfile)
+
+	profNumber.push(userProfile);
+
+	
 	var arrayProf = JSON.stringify(profNumber);
-	localStorage.setItem("Profile", arrayProf);
-	console.log(JSON.parse(localStorage.getItem("Profile")));
+	
+	localStorage.setItem("User", arrayProf);
+	console.log(JSON.parse(localStorage.getItem("User")));
+	
 }	
 
 
+function userLogin(){
+	var accounts = JSON.parse(localStorage.getItem("User"))
+	
+	for(i=0; accounts.length >= i; i++){
+		var acc = accounts[i];
+		if((document.getElementById("UserValue").value === acc.userIn) && 
+		(document.getElementById("userPass").value === acc.passIN)){
+			accepted = true;
+			break;
+		} else{
+			accepted = false; 
+		}
+		
+	}
+	console.log(accepted);
+	
+}
 
 
-
-
- /*function profcreate(){
-	user(id).firstName = document.getElementById("fn").value
-	user(id).lastName = document.getElementById("ln").value
-	user(id).usrname = document.getElementById("un").value
-	user(id).email = document.getElementById("email").value
-	user(id).passWord = document.getElementById("pass").value
-	console.log(user)
-	id += 1; 
- }*/
  
  
  
